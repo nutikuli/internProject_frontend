a1412c0114b843b325d273bca0f9e5bb16a3bd5a
NapatBourban <napatone123@gmail.com>
2024-05-24 14:18:06 +0700

    Login Page

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <script>
 import logo from "/src/lib/image/cyberpunk.jpg"
 </script>
@@ -50,6 +50,4 @@ import logo from "/src/lib/image/cyberpunk.jpg"
 
  
 </stype>
-=======
-<h1>hello</h1>
->>>>>>> 0ea4c8828ba3288111526702e634b5f2e67b729a
+
