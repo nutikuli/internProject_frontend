41affc82cfe4dc336f57b738b8c92a23b15c5333
Sunz098 <119925492+Sunz098@users.noreply.github.com>
2024-05-24 23:31:09 +0700

    เพิ่มuiหน้าadminทุกหน้า

--- src/routes/(admin)/admin/[adminId]/customer-management/+page.svelte
+++ src/routes/(admin)/admin/[adminId]/customer-management/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import Table from "../../../../../components/Table.svelte";
+</script>
+
 <div class="w-100  min-vh-100 bg-secondary-subtle ">
 	<div class="d-flex">
 	<div class="w-100 bg-white ">
@@ -5,7 +9,7 @@
             <span style="font-size: 30px;">สิทธิ์ผู้ใช้งาน</span>
             <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    เพิ่มสิทธื์ผู้ใช้งาน
+    เพิ่มสิทธิ์ผู้ใช้งาน
   </button>
   
   <!-- Modal -->
@@ -129,150 +133,25 @@
         </div>
       </form>
     </div>
-    <div class="w-100 bg-white shadow d-flex justify-content-between align-items-center px-3 py-2">
-      <table class="table">
-        <thead>
-          <tr class="table-primary">
-            <th scope="col">#</th>
-            <th scope="col">ชื่อบทบาท</th>
-            <th scope="col">เมนูที่เข้าถึงได้</th>
-            <th scope="col">Title</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>ผู้ดูแลรับบ</td>
-            <td>แดชบอร์ด,บทบาท,ผู้ดูแลระบบ,สมาชิก,Log</td>
-            <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-              xxx
-            </button>
-            
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">สิทธิ์ผู้ใช้งาน</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                      <div class="mb-3 row">
-                          <label for="inputPassword" class="col-sm-2 col-form-label">ชื่อสิทธิ์ : </label>
-                          <div class="col-sm-10">
-                            <input type="text" placeholder="placeholder" class="form-control" id="inputPassword">
-                          </div>
-                        </div>
-                        <table class="table">
-                          <thead>
-                            <tr class="table-primary">
-                              <th scope="col">รายการ</th>
-                              <th scope="col"></th>
-                              <th scope="col"></th>
-                              <th scope="col">เข้าถึงเมนู</th>
-                              
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>หน้าแรก</td>
-                              <td></td>
-                              <td></td>
-                              <td>
-                                  <div class="form-check  d-flex" style="margin-left: 20px;" >
-                                  <input class="form-check-input" type="checkbox" value="" id="firstpagecheckbox">
-                                </div>
-                              </td>
-                            </tr>
-                            
-                            <tr>
-                              <td>สิทธิ์ผู้ใช้งาน</td>
-                              <td></td>
-                              <td></td>
-                              <td>
-                                  <div class="form-check  d-flex" style="margin-left: 20px;" >
-                                  <input class="form-check-input" type="checkbox" value="" id="authencheckbox">
-                                </div>
-                              </td>
-                          
-                            </tr>
-                            <tr>
-                              <td>ผู้ดูแลระบบ</td>
-                              <td></td>
-                              <td></td>
-                              <td>
-                                  <div class="form-check  d-flex" style="margin-left: 20px;" >
-                                  <input class="form-check-input" type="checkbox" value="" id="managementcheckbox">
-                                </div>
-                              </td>
-                          
-                            </tr>
-                            <tr>
-                              <td>ร้านค้า</td>
-                              <td></td>
-                              <td></td>
-                              <td>
-                                  <div class="form-check  d-flex" style="margin-left: 20px;" >
-                                  <input class="form-check-input" type="checkbox" value="" id="stroecheckbox">
-                                </div>
-                              </td>
-                          
-                            </tr>
-                            <tr>
-                              <td>ลูกค้า</td>
-                              <td></td>
-                              <td></td>
-                              <td>
-                                  <div class="form-check  d-flex" style="margin-left: 20px;" >
-                                  <input class="form-check-input" type="checkbox" value="" id="customercheck">
-                                </div>
-                              </td>
-                          
-                            </tr>
-                            <tr>
-                              <td>log</td>
-                              <td></td>
-                              <td></td>
-                              <td>
-                                  <div class="form-check  d-flex" style="margin-left: 20px;" >
-                                  <input class="form-check-input" type="checkbox" value="" id="logcheckbox">
-                                </div>
-                              </td>
-                          
-                            </tr>
-                          </tbody>
-                        </table>
-          
-          
-          
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">บันทึก</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    
-                  </div>
-                  
-                </div>
-              </div>
-            </div></td>
-            <td><button class="btn btn-primary">lll</button></td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
+    <Table/> 
+    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" style="display: flex; justify-content:flex-end">
+      <div class="btn-group me-2" role="group" aria-label="First group" >
+        <button type="button" class="btn btn-outline-primary">icon</button>
+        <button type="button" class="btn btn-outline-primary">1</button>
+        <button type="button" class="btn btn-outline-primary">2</button>
+        <button type="button" class="btn btn-outline-primary">3</button>
+        <button type="button" class="btn btn-outline-primary">4</button>
+        <button type="button" class="btn btn-outline-primary">5</button>
+        <button type="button" class="btn btn-outline-primary">6</button>
+        <button type="button" class="btn btn-outline-primary">7</button>
+        <button type="button" class="btn btn-outline-primary">8</button>
+        <button type="button" class="btn btn-outline-primary">9</button>
+        <button type="button" class="btn btn-outline-primary">10</button>
+        <button type="button" class="btn btn-outline-primary">icon</button>
+      </div>
     </div>
-
+  </div>
+  
     <div>
 
     </div>
@@ -282,4 +161,4 @@
 
 	</div>
 </div>
-</div>
+
