e214016928876db4bd0fa09054dfd328ead70817
NapatBourban <napatone123@gmail.com>
2024-05-24 14:09:51 +0700

    Login page

---
+++ src/routes/(login)/login/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+import logo from "/src/lib/image/cyberpunk.jpg"
+</script>
+
+
+
+<div class="content-center" style="display:flex;justify-content:start; background-image:url('/src/lib/image/cyberpunk.jpg');height:100vh;">
+    <div style="background-color:#fff;width:30%">
+        <div class="d-flex justify-content-center" style="margin-top:20%">
+            <img src="{logo}" style="width:140px;height:140px;border-radius:75%">
+        </div>
+        <div style="text-align:center;margin-top:10px" > 
+        <h5 >เข้าสู่ระบบ</h5>
+        <h6 style="font-size:10px;margin-top:12px">ยินดีต้อนรับเข้าสู่ เว็บไซต์ Payso Shop</h6>
+        </div>
+        <div style="margin:20px 70px 20px 70px" >
+            <h6 style="font-size:16px;">อีเมล :</h6>
+            <div>
+            <input type="email" id="email" name="email" placeholder="Example@example.com" style="margin: 0px 0px 10px 0px;border-radius:8px;width:100%;height:40px" >
+            </div>
+            <div>
+            <h6 style="font-size:16px;">รหัสผ่าน :</h6>
+            <input  type="password" id="password" name="password" placeholder="Password"  style="border-radius:8px;width:100%;height:40px">
+            </div>
+            <div style="margin-top:10px" class="d-flex w-100">
+                <input type="checkbox"><h6 style="font-size:12px;margin-top:6px">จดจำฉันไว้ในระบบ</h6>
+                <a style="font-size:12px;margin-left:45%" class="mt-1" >ลืมรหัสผ่าน ?</a>
+                
+            </div>
+           
+            <button style="width:100%;margin-top:10px;border-radius:10px;height:40px">asdasda</button>
+            <div class="d-flex justify-content-center">
+            <h6 style="font-size:13px;margin-top:35px">ยังไม่มีบัญชีใช่หรือไม่ ?<a style="margin-left:10px" >สมัครบัญชี</a></h6>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div style="border-radius:50px" class="bg-secondary"></div>
+                <div style="border-radius:50px" class="bg-secondary"></div>
+                <div style="border-radius:50px" class="bg-secondary"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<stype scoped>
+
+ 
+</stype>
