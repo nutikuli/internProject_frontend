5c30dbb7a38f3ed539f9e43d6d91a6f31f684efe
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-27 21:52:16 +0700

    feat(table-ui): update table to table-data for query data functionality and pagination

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -3,6 +3,9 @@
 <script>
 	import Icon from '@iconify/svelte';
 	import Model from './Model.svelte';
+	import { onMount } from 'svelte';
+	import DataTable from 'datatables.net-dt';
+
 	// สร้างตัวแปร colLabels และกำหนดค่าเริ่มต้น
 	/** @type {string[]} */
 	export let colLabels = ['#', 'ลำดับงาน', 'เมนูที่เข้าถึงได้', 'หมวดหมู่', 'สถานะ', 'Actions'];
@@ -19,8 +22,12 @@
 		['โปรแกรมเมอร์', 'แอดมิน, มาตรา, ผู้บังคับบัญชา, สมาชิค, Log', 'AS', 'ใช้งาน'],
 		['ผู้บริหาร', 'แอดมิน, มาตรา, ผู้บังคับบัญชา, สมาชิค, Log', 'AS', 'ปิดใช้งาน'],
 		['พนักงานทั่วไป', 'แอดมิน, มาตรา, ผู้บังคับบัญชา, สมาชิค, Log', 'AS', 'ปิดใช้งาน'],
+		['พนักงานชั่วคราว', 'แอดมิน, มาตรา, ผู้บังคับบัญชา, สมาชิค, Log', 'AS', 'ปิดใช้งาน'],
+		['พนักงานชั่วคราว', 'แอดมิน, มาตรา, ผู้บังคับบัญชา, สมาชิค, Log', 'AS', 'ปิดใช้งาน'],
 		['พนักงานชั่วคราว', 'แอดมิน, มาตรา, ผู้บังคับบัญชา, สมาชิค, Log', 'AS', 'ปิดใช้งาน']
 	];
+
+	export let tableTarget = 'table-data';
 
 	const editRow = (index) => {
 		alert(`Edit row ${index + 1}`);
@@ -33,10 +40,116 @@
 	function badgeStatus(status) {
 		return status[0] === 'ใช้งาน' ? 'badge bg-success' : 'badge bg-danger';
 	}
+
+	function clear() {
+		const inputElement = document.querySelector('.dt-input');
+		if (inputElement) {
+			// @ts-ignore
+			inputElement.value = '';
+		}
+	}
+
+	function onLoadCustomPaginationStyle() {
+		const dtPagingDiv = document.querySelector('.dt-paging.paging_full_numbers');
+		if (dtPagingDiv) {
+			dtPagingDiv.classList.add('pagination');
+
+			const buttons = dtPagingDiv.querySelectorAll('button');
+			buttons.forEach((button) => {
+				const aTag = document.createElement('a');
+				aTag.className = 'page-link';
+				aTag.textContent = button.textContent;
+
+				button.className = 'page-item btn btn-outline m-0 p-0';
+				button.textContent = '';
+				button.addEventListener('click', onLoadCustomPaginationStyle);
+
+				button.appendChild(aTag);
+
+				if (button.getAttribute('aria-current') === 'page') {
+					button.classList.add('active');
+				}
+			});
+		}
+	}
+
+	function tableTriggerEvent(table) {
+		table.on('search.dt', function () {
+			setTimeout(() => {
+				onLoadCustomPaginationStyle();
+			}, 50);
+		});
+
+		table.on('order.dt', function () {
+			setTimeout(() => {
+				onLoadCustomPaginationStyle();
+			}, 50);
+		});
+	}
+
+	onMount(() => {
+		const table = new DataTable(`#${tableTarget}`, {
+			lengthChange: false,
+			searching: true,
+			ordering: true,
+			info: true,
+			searchDelay: 200,
+			language: {
+				search: 'ค้นหา:',
+				zeroRecords: 'ไม่พบข้อมูลที่ค้นหา',
+				info: 'แสดง _START_ ถึง _END_ จาก _TOTAL_ รายการ',
+				infoEmpty: 'ไม่พบข้อมูล',
+				infoFiltered: '(กรองจากทั้งหมด _MAX_ รายการ)'
+			},
+			autoWidth: false
+		});
+		tableTriggerEvent(table);
+
+		const dtLength = document.querySelector('.dt-length');
+		if (dtLength) {
+			// @ts-ignore
+			dtLength.style.display = 'none';
+		}
+
+		// modify the search bar
+		const dtSearchDiv = document.querySelector('.dt-search');
+		if (dtSearchDiv) {
+			dtSearchDiv.classList.add('d-flex', 'align-items-center', 'gap-3', 'my-3');
+
+			const inputElement = dtSearchDiv.querySelector('.dt-input');
+			if (inputElement) {
+				inputElement.classList.add('form-control');
+				inputElement.setAttribute('placeholder', 'search');
+			}
+			const labelElement = document.querySelector('label[for="dt-search-0"]');
+			if (labelElement) {
+				labelElement.classList.add('d-none');
+			}
+
+			const searchButton = document.createElement('button');
+			searchButton.textContent = 'ค้นหา';
+			searchButton.className = 'btn btn-primary px-4 py-2';
+			dtSearchDiv.appendChild(searchButton);
+
+			const clearButton = document.createElement('button');
+			clearButton.textContent = 'ล้างการค้นหา';
+			clearButton.className = 'btn px-4 py-2 btn-outline-primary';
+			clearButton.addEventListener('click', clear);
+			dtSearchDiv.appendChild(clearButton);
+		}
+
+		const dtPagingLayout = document.querySelectorAll('.dt-layout-row');
+
+		if (dtPagingLayout) {
+			dtPagingLayout[2].className = 'd-flex m-2 gap-4 justify-content-end align-items-center';
+		}
+		// modify the pagination
+		onLoadCustomPaginationStyle();
+	});
 </script>
 
 <div class="table-responsive">
-	<table class="table on-xl-overflow-x-hidden text-center table-striped">
+	<table id={tableTarget} style="width: 100%" class="table text-center table-striped">
 		<thead class="table-c-primary">
 			<tr>
 				{#each colLabels as c}
@@ -51,28 +164,30 @@
 					{#each record.slice(0, -1) as cell}
 						<td style="font-size: 0.90rem;">{cell}</td>
 					{/each}
-					<!--- สำหรับใช้กับ "สถานะ" ไม่ส่วนนี้ลบทึ้ง -->
+					<!--- สำหรับใช้กับ "สถานะ" ไม่ใช้ส่วนนี้ลบทึ้ง -->
 					<td>
 						<span class={badgeStatus(record.slice(-1))}>{record.slice(-1)}</span>
 					</td>
 					<td class="table-actions">
-						<button
-							data-bs-toggle="modal"
-							data-bs-target={`#modal-editor-${index}`}
-							class="btn btn-outline-warning btn-sm"
-							><Icon width="16" icon="material-symbols:edit-square-outline" /></button
-						>
-						<Model modalTargetId={`modal-editor-${index}`} modalTitle={'แก้ไขข้อมูล'} />
+						<slot name="actions">
+							<button
+								data-bs-toggle="modal"
+								data-bs-target={`#modal-editor-${index}`}
+								class="btn btn-outline-warning btn-sm"
+								><Icon width="16" icon="material-symbols:edit-square-outline" /></button
+							>
+							<Model modalTargetId={`modal-editor-${index}`} modalTitle={'แก้ไขข้อมูล'} />
 
-						<button
-							data-bs-toggle="modal"
-							data-bs-target={`#modal-delete-${index}`}
-							class="btn btn-outline-danger btn-sm"
-						>
-							<Icon width="16" icon="material-symbols:delete" />
-						</button>
+							<button
+								data-bs-toggle="modal"
+								data-bs-target={`#modal-delete-${index}`}
+								class="btn btn-outline-danger btn-sm"
+							>
+								<Icon width="16" icon="material-symbols:delete" />
+							</button>
 
-						<Model modalTargetId={`modal-delete-${index}`} modalTitle={'ลบข้อมูล'} />
+							<Model modalTargetId={`modal-delete-${index}`} modalTitle={'ลบข้อมูล'} />
+						</slot>
 					</td>
 				</tr>
 			{/each}
@@ -88,6 +203,13 @@
 	.table-striped > tbody > tr:nth-child(2n + 1) > td,
 	.table-striped > tbody > tr:nth-child(2n + 1) > th {
 		background-color: rgba(#f8f9fa, 0.5);
+	}
+
+	div.dt-length {
+		display: none !important;
+	}
+
+	label.dt-length-0 {
 	}
 
 	@media (max-width: 1024px) {
