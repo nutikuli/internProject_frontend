052933f23a3e7a28a5f72c84907103f66eac0944
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-05-27 12:54:41 +0700

    แก้ไข folder (ลบ detail ย้าย code ไปไว้ใน [productId], ย้าย code ใน folder productId ไปไว้ใน product)

--- src/routes/(customer)/product/[productId]/+page.svelte
+++ src/routes/(customer)/product/[productId]/+page.svelte
@@ -1,75 +1,78 @@
 <script>
-	import Icon from '@iconify/svelte';
 	import NavbarCustomer from '../../../../components/navbarCustomer.svelte';
-	function gotoDetail() {
-		window.location.href = '/detail/id/id';
-	}
 </script>
 
 <div>
 	<NavbarCustomer />
-	<div class="showProduct">
-		<!-- sort -->
-		<div style="display: flex; padding-bottom: 20px">
-			<span style="margin-right: 10px; margin-top: 10px;">จัดเรียงตาม</span>
-
-			<select class="form-select" id="input" style="width: 200px;">
-				<option selected>--select--</option>
-				<option value="1">ราคา มากไปน้อย</option>
-				<option value="2">ราคา น้อยไปมาก</option>
-				<option value="3">สินค้าล่าสุด</option>
-				<option value="4">สินค้าเก่าสุด</option>
-			</select>
-		</div>
-
-		<!-- sort -->
-		<!-- productList -->
-		<div class="card" style="width: 18rem;" on:click={gotoDetail}>
-			<img src="/src/lib/image/cyberpunk.jpg" class="card-img-top" alt="..." />
-			<div class="card-body">
-				<h5 class="card-title">Product Name</h5>
-				<p class="card-text">
-					Some quick example text to build on the card title and make up the bulk of the card's
-					content.
-				</p>
-				<div class="row justify-content-between">
-					<div class="col-auto d-flex align-items-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="25"
-							height="25"
-							viewBox="0 0 24 24"
-							{...$$props}
-						>
-							<path
-								fill="none"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="3"
-								d="M8 6h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 12H8m0 0h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 18H8M8 6v12m3-14v2m0 12v2"
-							/>
-						</svg>
-						<h2 style="font-weight: 700;">551</h2>
+	<div style="padding-left: 60px; padding-right: 60px; padding-top: 30px;">
+		<div class="card">
+			<div class="row">
+				<!-- image -->
+				<div class="col-md-4">
+					<!-- First image -->
+					<img src="/src/lib/image/cyberpunk.jpg" class="card-img" alt="..." />
+					<!-- Group of three images -->
+					<div class="row" style="padding-top: 10px;">
+						<div class="col-4">
+							<img src="/src/lib/image/cyberpunk.jpg" class="card-img mb-3" alt="..." />
+						</div>
+						<div class="col-4">
+							<img src="/src/lib/image/cyberpunk.jpg" class="card-img mb-3" alt="..." />
+						</div>
+						<div class="col-4">
+							<img src="/src/lib/image/cyberpunk.jpg" class="card-img mb-3" alt="..." />
+						</div>
 					</div>
-					<div class="col-auto">
-						<a href="#" class="btn btn-primary">ซื้อเลย</a>
+				</div>
+
+				<div class="col-md-8">
+					<h3 class="text-left mb-3">Product Nameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</h3>
+					<div class="d-flex align-items-center justify-content-between">
+						<div>
+							<div class="d-flex">
+								<h5 style="margin-right: 34px;">หมวดหมู่</h5>
+								<div>smd,sd</div>
+							</div>
+							<div class="d-flex">
+								<h5 style="margin-right: 50px;">ร้านค้า</h5>
+								<div>smd,sd</div>
+							</div>
+							<div class="d-flex">
+								<div style="margin-right: 60px; margin-top: 10px;">จำนวน</div>
+								<input type="text" class="form-control w-100 mr-3" placeholder="Input" />
+								<div style="margin-left: 10px; margin-top: 10px;">ชิ้น</div>
+							</div>
+						</div>
+						<div>
+							<div class="d-flex">
+								<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+									<path
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="3"
+										d="M8 6h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 12H8m0 0h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 18H8M8 6v12m3-14v2m0 12v2"
+									/>
+								</svg>
+								<h2 style="font-weight: 700;">551</h2>
+							</div>
+
+							<div class="col-auto">
+								<a href="#" class="btn btn-primary">ใส่ตะกร้า</a>
+							</div>
+						</div>
 					</div>
+                    <br>
+                    <div>Detail</div>
 				</div>
 			</div>
 		</div>
-		<!-- productList -->
 	</div>
 </div>
 
 <style>
-	.showProduct {
-		margin-top: 30px;
-		margin-left: 80px;
-		margin-right: 80px;
-		align-items: center;
-	}
-	.bi {
-		margin-top: 5px;
+	.card {
+		padding: 10px;
 	}
 </style>
