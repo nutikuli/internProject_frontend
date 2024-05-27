be8d7cbfab8cc935175a27843f21da9b05e6b026
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-27 23:02:59 +0700

    feat(store-order): Add order management page to store layout

---
+++ src/routes/(store)/store/[storeId]/order-management/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Table from '../../../../../components/Table.svelte';
+</script>
+
+<div id="product-managment-container" class="container-fluid">
+	<div class="d-flex justify-content-between align-items-center">
+		<span class="fs-2 fw-bold">รายการธุรกรรม</span>
+		<button
+			type="button"
+			class="btn btn-primary  px-3 py-2"
+			data-bs-toggle="modal"
+			data-bs-target="#exampleModal"
+		>
+			เพิ่มรายการธุรกรรม
+		</button>
+	</div>
+	<Table></Table>
+</div>
