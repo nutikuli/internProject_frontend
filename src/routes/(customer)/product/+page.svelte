22ff77f49751d73ce0d4b475fe92cbace49aa2cb
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-06-03 16:33:57 +0700

    fix : product list

--- src/routes/(customer)/product/+page.svelte
+++ src/routes/(customer)/product/+page.svelte
@@ -8,23 +8,23 @@
 	import { loadAll } from './+page.server.js';
 	import { onMount } from 'svelte';
 
-  let products = [];
-  let error = null;
+	let products = [];
+	let error = null;
 
-  // Define a method to load all products
-  async function loadProducts() {
-    const { products: loadedProducts, error: loadingError } = await loadAll();
-    if (loadingError) {
-      // Handle error if there's any
-      error = loadingError;
-    } else {
-      // Set the products if loaded successfully
-      products = loadedProducts;
-    }
-  }
+	// Define a method to load all products
+	async function loadProducts() {
+		const { products: loadedProducts, error: loadingError } = await loadAll();
+		if (loadingError) {
+			// Handle error if there's any
+			error = loadingError;
+		} else {
+			// Set the products if loaded successfully
+			products = loadedProducts;
+		}
+	}
 
-  // Call the loadProducts method when the component is mounted
-  onMount(loadProducts);
+	// Call the loadProducts method when the component is mounted
+	onMount(loadProducts);
 </script>
 
 <div>
@@ -45,39 +45,42 @@
 
 		<!-- sort -->
 		<!-- productList -->
-		{#each products as product}
-		<div class="card" style="width: 18rem;" on:click={gotoDetail}>
-			<img src="/src/lib/image/cyberpunk.jpg" class="card-img-top" alt="..." />
-			<div class="card-body">
-				<h5 class="card-title">{product.name}</h5>
-				<p class="card-text">{product.detail}</p>
-				<div class="row justify-content-between">
-					<div class="col-auto d-flex align-items-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="25"
-							height="25"
-							viewBox="0 0 24 24"
-							{...$$props}
-						>
-							<path
-								fill="none"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="3"
-								d="M8 6h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 12H8m0 0h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 18H8M8 6v12m3-14v2m0 12v2"
-							/>
-						</svg>
-						<h2 style="font-weight: 700;">{product.price}</h2>
-					</div>
-					<div class="col-auto">
-						<a href="#" class="btn btn-primary">ซื้อเลย</a>
+		<div class="d-flex">
+			{#each products as product}
+				<div class="card" style="width: 18rem;" on:click={gotoDetail}>
+					<img src="/src/lib/image/cyberpunk.jpg" class="card-img-top" alt="..." />
+					<div class="card-body">
+						<h5 class="card-title">{product.name}</h5>
+						<p class="card-text">{product.detail}</p>
+						<div class="row justify-content-between">
+							<div class="col-auto d-flex align-items-center">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="25"
+									height="25"
+									viewBox="0 0 24 24"
+									{...$$props}
+								>
+									<path
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="3"
+										d="M8 6h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 12H8m0 0h5a3 3 0 0 1 3 3v.143A2.857 2.857 0 0 1 13.143 18H8M8 6v12m3-14v2m0 12v2"
+									/>
+								</svg>
+								<h2 style="font-weight: 700;">{product.price}</h2>
+							</div>
+							<div class="col-auto">
+								<a href="#" class="btn btn-primary">ซื้อเลย</a>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			{/each}
 		</div>
-		{/each}
+
 		<!-- productList -->
 	</div>
 </div>
