29eebd4c9c66cf070ae666e02ba1799c942de0b0
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-06-05 15:11:59 +0700

    fix : seller and detail

--- src/routes/customer/product/[productId]/+page.svelte
+++ src/routes/customer/product/[productId]/+page.svelte
@@ -8,12 +8,18 @@
 	const productName = data.product.result.product_data.name;
 	const productDetail = data.product.result.product_data.detail;
 	const productPrice = data.product.result.product_data.price;
-	const productProductAvatar = data.product.result.product_data.product_avatar;
-	const productProductImage = data.product.result.files_data;
+	const productAvatar = data.product.result.product_data.product_avatar;
 	const productStock = data.product.result.product_data.stock;
 	const productCategoryId = data.product.result.product_data.categoryId;
 	const productStoreId = data.product.result.product_data.storeId;
-	let imageData = [{ productProductImage }];
+	const filesData = data.product.result.files_data;
+	console.log(filesData);
+
+	let activeIndex = 0;
+
+	function showThumbnail(index) {
+		activeIndex = index;
+	}
 </script>
 
 <div>
@@ -22,9 +28,41 @@
 		<div style="padding-left: 60px; padding-right: 60px; padding-top: 30px;">
 			<div class="card">
 				<div class="row">
-					<div class="main-img">
-						<img src={productProductAvatar} alt={productName} />
+					<!-- test image -->
+					<div id="carouselAutoplaying" class="carousel slide">
+						<div class="carousel-inner">
+							{#each filesData as file, index}
+								<div class="carousel-item {index === activeIndex ? 'active' : ''}">
+									<img
+									style="margin: 10px;"
+										src={file.file_data ||
+											'https://resource.logitech.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/mice/m240/product-gallery/m240-mouse-top-view-graphite.png?v=1'}
+										alt={file.file_name}
+									/>
+								</div>
+							{/each}
+						</div>
+						<div class="carousel-thumbnails d-flex">
+							{#each filesData as file, index}
+								<button
+									type="button"
+									class="thumbnail {index === activeIndex ? 'active' : ''}"
+									data-bs-target="#carouselAutoplaying"
+									data-bs-slide-to={index}
+									style="border: 0cm;"
+									on:click={() => showThumbnail(index)}
+								>
+									<img
+										class="img-thumbnail"
+										src={file.file_data ||
+											'https://resource.logitech.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/mice/m240/product-gallery/m240-mouse-top-view-graphite.png?v=1'}
+										alt={file.file_name}
+									/>
+								</button>
+							{/each}
+						</div>
 					</div>
+					<!-- test image -->
 					<div class="col-md-8">
 						<h3 class="text-left mb-3">{productName}</h3>
 						<div class="d-flex align-items-center justify-content-between">
@@ -50,7 +88,7 @@
 								</div>
 
 								<div class="col-auto">
-									<a href="#" class="btn btn-primary">ใส่ตะกร้า</a>
+									<a href="/customer/customer-cart/id" class="btn btn-primary">ใส่ตะกร้า</a>
 								</div>
 							</div>
 						</div>
@@ -64,16 +102,50 @@
 </div>
 
 <style>
-	.main-img {
+	.thumbnail {
+		cursor: pointer;
+		margin-right: 10px;
+	}
+
+	.img-thumbnail {
+		max-width: 100px;
+		max-height: 100px;
+	}
+
+	.carousel-item {
+		transition: transform 0.5s ease;
+	}
+
+	.carousel-thumbnails {
+		margin-top: 10px;
+	}
+
+	.carousel-item.active {
+		display: block;
+	}
+
+	.thumbnail.active img {
+		border: 1px solid #007bff;
+	}
+	.carousel,
+	.carousel-inner,
+	.carousel-item {
 		width: min-content;
 	}
 	img {
-		width: 350px;
+		width: 200px;
 		height: auto;
 	}
 
 	.btn {
 		display: block;
 		margin-top: 20px;
+	}
+	/* ---------------- */
+	.img-thumbnail {
+		width: 100px;
+	}
+	.carousel-thumbnails {
+		margin-left: 25px;
 	}
 </style>
