1cc4412d3d9aef8e2fcade8bc67a0340ce87b80d
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-06-12 10:29:59 +0700

    feat: Add new types for Bank, Product, Order, and Result
    - Added new typedefs for Bank, BankResult, ProductData, ProductResult, ProductWithProductOrderResult, OrderResult, and Order
    - These typedefs define the structure of the corresponding data objects used in the application

--- src/lib/internal/types.js
+++ src/lib/internal/types.js
@@ -26,6 +26,81 @@
  */
 
 /**
+ * @typedef {Object} Bank
+ * @property {number} id
+ * @property {string} name
+ * @property {string} acc_number
+ * @property {string} acc_name
+ * @property {string} avatar_url
+ * @property {string} status
+ * @property {number} store_id
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
+/**
+ * @typedef {Object} BankResult
+ * @property {FileData[]} files_data
+ * @property {Bank} bank_data
+ */
+
+/**
+ * @typedef {Object} ProductData
+ * @property {number} id
+ * @property {string} name
+ * @property {number} price
+ * @property {number} stock
+ * @property {string} detail
+ * @property {boolean} status
+ * @property {string} product_avatar
+ * @property {number} category_id
+ * @property {number} store_id
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
+/**
+ * @typedef {Object} ProductResult
+ * @property {FileData[]} files_data
+ * @property {ProductData} product_data
+ * @property {ProductCategoryData} product_category_data
+ */
+
+/**
+ * @typedef {Object} ProductWithProductOrderResult
+ * @property {FileData[]} files_data
+ * @property {ProductData} product_data
+ * @property {ProductCategoryData} product_category_data
+ * @property {import("$lib/pkg/utils/cookies").OrderProduct} order_product_data
+ */
+
+/**
+ * @typedef {Object} OrderResult
+ * @property {Order} order_data
+ * @property {import("$lib/pkg/utils/cookies").Account} customer_data
+ * @property {FileData[]} files_data
+ * @property {BankResult} bank_payment
+ * @property {ProductWithProductOrderResult[]} product_data
+ */
+
+/**
+ * @typedef {Object} Order
+ * @property {number} id
+ * @property {number} total_amount
+ * @property {string} topic
+ * @property {number} sum_price
+ * @property {string} state
+ * @property {string} delivery_type
+ * @property {string} parcel_number
+ * @property {string} sent_date
+ * @property {number} customer_id
+ * @property {number} store_id
+ * @property {number} bank_id
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
+/**
  * @typedef {Object} Result
  * @property {FileData[]} files_data
  */
